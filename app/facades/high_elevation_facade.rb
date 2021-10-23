479bfeee640d47e23b1cc4ef20a1b148365392ed
Jacob Martinez <jacobmarcareer@gmail.com>
2021-10-23 11:13:13 -0600

    Clean up code for recipe response

--- app/facades/high_elevation_facade.rb
+++ app/facades/high_elevation_facade.rb
@@ -1,7 +1,15 @@
 class HighElevationFacade
   class << self
-    def adjust_recipe(recipes)
-      recipes.ingredients.adjusted_math
+    def adjust_ingredient(ingredient)
+      if ingredient.title.downcase.include?('sugar') && ingredient.unit.include?('cup')
+        "#{ingredient.quantity} #{ingredient.unit} #{ingredient.adjusted_sugar(ingredient)}"
+      elsif ingredient.title.downcase.include?('flour') && ingredient.unit.include?('cup')
+        "#{ingredient.quantity} #{ingredient.unit} #{ingredient.adjusted_flour(ingredient)}"
+      elsif ingredient.title.downcase.include?('baking') && (ingredient.title.downcase.include?("soda") || ingredient.title.downcase.include?("powder"))
+        "#{ingredient.adjusted_leavening(ingredient)} #{ingredient.unit}"
+      else
+        "#{ingredient.quantity} #{ingredient.unit}"
+      end
     end
   end
 end
--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -3,9 +3,11 @@ class RecipeSerializer
   attributes :title, :description, :category, :cook_time
 
   attributes :ingredients do |object|
+    hash = {}
     object.ingredients.map do |ingredient|
-      "#{ingredient.title}: #{ingredient.quantity} #{ingredient.unit}"
+      hash[ingredient.title] = "#{ingredient.quantity} #{ingredient.unit}"
     end
+    hash
   end
 
   attributes :images do |object|
