a43d33afc66be2b74033893e2cf3964c9748d2e3
Jacob Martinez <jacobmarcareer@gmail.com>
2021-10-23 11:13:13 -0600

    Clean up code for recipe response

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -8,21 +8,17 @@ class Ingredient < ApplicationRecord
 
   def adjusted_sugar(sugar)
     tbsp = sugar.quantity.to_f
-    sugar.quantity = "#{sugar.quantity} - #{tbsp} tablespoons"
+    "- #{tbsp} tablespoons"
   end
 
-  def adjusted_baking_powder(powder)
-    reduction = powder.quantity.to_f - (powder.quantity.to_i * 0.125)
-    powder.quantity = reduction.to_r.divmod(1).split(",").join(" ").to_s
-  end
-
-  def adjusted_baking_soda(soda)
-    reduction = soda.quantity.to_f - (soda.quantity.to_i * 0.125)
-    soda.quantity = reduction.to_r.divmod(1).split(",").join(" ").to_s
+  def adjusted_leavening(ingredient)
+    reduction = ingredient.quantity.to_f - (ingredient.quantity.to_i * 0.125)
+    ingredient.quantity = reduction.to_r.divmod(1).split(",").join(" ").to_s
+    ingredient.quantity.delete_prefix("0 ")
   end
 
   def adjusted_flour(flour)
     tbsp = flour.quantity.to_f
-    flour.quantity = "#{flour.quantity} + #{tbsp} tablespoons"
+    "+ #{tbsp} tablespoons"
   end
 end
