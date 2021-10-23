b9241431e00e0465abeba646485ce0e93f07bb3e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-10-23 13:59:13 -0600

    Allow surge frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+  allow do
+    origins 'http://lohi.surge.sh/#/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
