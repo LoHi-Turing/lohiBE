c0625164ee2bf8190124c2b20109d54345740d82
Jacob Piland <piland.jacob@gmail.com>
2021-11-06 22:24:55 -0500

    Add user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_15_012615) do
+ActiveRecord::Schema.define(version: 2021_11_07_031517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2021_10_15_012615) do
     t.text "description", default: [], array: true
     t.string "category"
     t.string "cook_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
