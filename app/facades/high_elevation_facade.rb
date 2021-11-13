b02c3ef50d28c128df60457e802c6d1236c8a710
Jacob Martinez <jacobmarcareer@gmail.com>
2021-11-13 12:48:31 -0700

    Add in elevation

--- app/facades/high_elevation_facade.rb
+++ app/facades/high_elevation_facade.rb
@@ -1,12 +1,12 @@
 class HighElevationFacade
   class << self
-    def adjust_ingredient(ingredient)
+    def adjust_ingredient(ingredient, elevation)
       if ingredient.title.downcase.include?("sugar") && ingredient.unit.include?("cup")
-        "#{ingredient.quantity} #{ingredient.unit} #{ingredient.adjusted_sugar(ingredient)}"
+        "#{ingredient.quantity} #{ingredient.unit} #{ingredient.adjusted_sugar(ingredient, elevation)}"
       elsif ingredient.title.downcase.include?("flour") && ingredient.unit.include?("cup")
         "#{ingredient.quantity} #{ingredient.unit} #{ingredient.adjusted_flour(ingredient)}"
       elsif ingredient.title.downcase.include?("baking") && (ingredient.title.downcase.include?("soda") || ingredient.title.downcase.include?("powder"))
-        "#{ingredient.adjusted_leavening(ingredient)} #{ingredient.unit}"
+        "#{ingredient.adjusted_leavening(ingredient, elevation)} #{ingredient.unit}"
       else
         "#{ingredient.quantity} #{ingredient.unit}"
       end
