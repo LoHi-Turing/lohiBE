94efe3bac81393b8d03703da79d070e7da606c7b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-11-13 12:48:17 -0700

    Add new elevation break off point

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     recipes = Recipe.all
-    if params[:elevation].to_i < 5000
+    if params[:elevation].to_i < 3500
       render json: RecipeSerializer.new(recipes)
     else
-      render json: RecipeHighSerializer.new(recipes)
+      render json: RecipeHighSerializer.new(recipes, {params: {elevation: params[:elevation]}})
     end
   end
 
